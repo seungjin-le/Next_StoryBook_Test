6845fe49d15330b769a05d4cbc06ddbed0a3b69f
Lee Seung Jin <dltmdwls154@gmail.com>
2024-02-15 23:56:33 +0900

    1Day 1Commit

--- src/stories/components/buttons/Button.stories.js
+++ src/stories/components/buttons/Button.stories.js
@@ -1,6 +1,5 @@
 import { Button } from './Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
   title: 'Components/Button',
   component: Button,
